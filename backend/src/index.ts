ada9ba83b9c270328f02894966b00b05d8ee72f3
rampemus <pasi.toivanen@live.com>
2019-11-15 16:12:46 +0200

    uses bodyparser and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
-
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import express from 'express'
-const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (request, respond) => {
+  respond.send('<h1>Hello world</h1>')
 })
 
 const PORT = process.env.PORT || 3001

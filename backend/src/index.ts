570db3f06ad4acecec87e69da43699956a91ac90
rampemus <pasi.toivanen@live.com>
2019-11-15 15:59:30 +0200

    Linter updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,12 @@
 
-import express from "express";
-const app = express();
+import express from 'express'
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello world</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})

50dec0d1f0a1cd7c33345b5ce30fb2a7e216f6d0
rampemus <pasi.toivanen@live.com>
2019-11-15 14:02:00 +0200

    Added create-react-app frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send("<h1>Hello world</h1>");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
     console.log(`Server running on port ${PORT}`);

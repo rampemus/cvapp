bb0713d9fe90049e4fc8f9cd0c3c8c068cbbaa35
rampemus <pasi.toivanen@live.com>
2020-01-10 14:19:58 +0200

    ts: unused import removed

--- backend/src/controllers/cv.ts
+++ backend/src/controllers/cv.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Response, Router } from 'express'
 import Communication from '../models/cv/communication'
 import Contact from '../models/cv/contact'
 import CurriculumVitae from '../models/cv/cv'

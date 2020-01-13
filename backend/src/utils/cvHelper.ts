564c191325fef9b71f2b6f32c6c2f47fec20af2e
rampemus <pasi.toivanen@live.com>
2020-01-13 22:25:55 +0200

    createTestCV for testing and preview

--- backend/src/utils/cvHelper.ts
+++ backend/src/utils/cvHelper.ts
@@ -123,6 +123,130 @@ const disconnectObjectFromCVField = async (cv: string, field: string, object: st
     }
 }
 
+const createTestCV = async (username: string) => {
+    const owner = await User.findOne({ username })
+
+    const contact = new Contact({
+        address: 'Testaavankatu 23, 80200 Jopensuu',
+        company: 'Great company',
+        email: 'testaava.testi@testaus.net',
+        firstname: 'Teppo',
+        lastname: 'Testaavainen',
+        owner,
+        phone: '0452478903',
+        phoneAvailable: 'always',
+    })
+    const savedContact = await contact.save()
+
+    const reference = new Contact({
+        address: 'Tuikitavallinen 1, 00100 Helsinki',
+        company: 'Important company',
+        email: 'testattu.tasti@test.com',
+        firstname: 'Petri',
+        lastname: 'Pomo',
+        owner,
+        phone: '050974417869',
+        phoneAvailable: 'During holidays 1pm-4pm',
+    })
+    const savedReference = await reference.save()
+
+    const profile = new Profile({
+        content: ['Many feelings of feeling the feels of feeling may lead to feel a alot.', 'Note that there is feelings that are felt many times of feelings that the feelings create'],
+        name: 'Feelgoodjob profile',
+        owner
+    })
+    const savedProfile = await profile.save()
+
+    const experience = new Experience({
+        description: 'Many job done',
+        name: 'The average job',
+        owner,
+        timeFrame: {
+            endDate: new Date(),
+            startDate: new Date(),
+        },
+    })
+    const savedExperience = await experience.save()
+
+    const education = new Experience({
+        description: 'Many studies done',
+        name: 'The basic degree',
+        owner,
+        timeFrame: {
+            endDate: new Date(),
+            startDate: new Date(),
+        }
+    })
+    const savedEducation = await education.save()
+
+    const communication = new Communication({
+        content: ['I speak as Im spoken of that I spoke what I speak'],
+        languages: [
+            {
+                language: 'English',
+                level: 'Fluent',
+            },
+            {
+                language: 'Spook',
+                level: 'Spok',
+            }
+        ],
+        name: 'Bilanguagelist',
+        owner,
+    })
+    const savedCommunication = await communication.save()
+
+    const skills = new Info({
+        content: ['A lot of social superskills'],
+        name: 'Social skills',
+        owner,
+    })
+    const savedSkills = await skills.save()
+
+    const info = new Info({
+        content: ['Can drive a car, a boat, a truck and a plane', 'All that with just a one hand!'],
+        name: 'Driving info',
+        owner
+    })
+    const savedInfo = await info.save()
+
+    const attachments = new Info({
+        content: ['Certificate of studies', 'certificate of driving', 'Certificate of trucks'],
+        name: 'Certificates and that short of things',
+        owner
+    })
+    const savedAttachments = await attachments.save()
+
+    const project = new Project({
+        description: 'Project that will be soon finished',
+        githubUrl: 'https://github.com/rampemus/cvapp',
+        name: 'CV project',
+        owner,
+        showcaseUrl: 'http://localhost:3000/',
+        thumbnailUrl: 'logo.svg',
+    })
+    const savedProject = await project.save()
+
+    const cv = new CurriculumVitae({
+        attachments: savedAttachments,
+        communication: savedCommunication,
+        contact: savedContact,
+        education: [savedEducation],
+        experience: [savedExperience],
+        github: 'https://github.com/rampemus',
+        info: savedInfo,
+        name: 'Testing cv',
+        owner,
+        profile: savedProfile,
+        projects: [savedProject],
+        reference: [savedReference],
+        skills: savedSkills,
+        techlist: 'Java, Python, CSS, C#, Angular',
+    })
+}
+
 export {
-    connectObjectToCVField
+    connectObjectToCVField,
+    disconnectObjectFromCVField,
+    createTestCV
 }

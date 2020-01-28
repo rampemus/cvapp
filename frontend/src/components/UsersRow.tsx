8ae582ffb011da756a4ddcdabf8c57bb76a06af3
rampemus <pasi.toivanen@live.com>
2020-01-28 13:01:54 +0200

    Logic replaced by css

--- frontend/src/components/UsersRow.tsx
+++ frontend/src/components/UsersRow.tsx
@@ -10,15 +10,13 @@ interface Props {
 const UsersRow: React.FC<Props> = (props) => {
     const { name, username, id, created, expires } = props.user
 
-    const [hovered, setHovered] = useState<boolean>(false)
-
     return (
-        <tr key={'usertr' + id} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+        <tr key={'usertr' + id}>
             <td><Link to={`/users/${username}`}>{username}</Link></td>
             <td>{name}</td>
             <td>{created.toString().substring(0, 10)} / {expires ? expires.toString().substring(0, 10) : '-never-'}</td>
             <td>{username === 'rampemus' ? 'Admin' : 'User'}</td>
-            <td style={{ opacity: hovered ? 1 : 0 }}><button onClick={()=>props.handleUserDelete()}>Delete</button></td>
+            <td><button onClick={()=>props.handleUserDelete()}>Delete</button></td>
         </tr>
     )
 }

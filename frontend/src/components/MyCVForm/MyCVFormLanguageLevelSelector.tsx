f2a4fbf7348563715b13a1a02f57b41ec7e34791
rampemus <pasi.toivanen@live.com>
2020-02-03 21:43:57 +0200

    Language level selection tool

---
+++ frontend/src/components/MyCVForm/MyCVFormLanguageLevelSelector.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+interface OwnProps {
+    initLevel: string,
+    handleChange(newLevel: string): void,
+}
+
+export enum ILevel {
+    Elementary = 'Elementary',
+    EveryDay = 'Every day proficiency',
+    Working = 'Working proficiency',
+    Advanced = 'Advanced',
+    Professional = 'Professional',
+    Native = 'Native speaker'
+}
+
+const levels = [ILevel.Elementary, ILevel.EveryDay, ILevel.Working, ILevel.Advanced, ILevel.Professional, ILevel.Native]
+
+const MyCVFormLanguageLevelSelector: React.FC<OwnProps> = (props) => {
+
+    const [level, setLevel] = useState(props.initLevel)
+
+    const handleLevelChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        event.preventDefault()
+
+        setLevel(event.target.value)
+        props.handleChange(event.target.value.toString())
+    }
+
+    return(
+        <div>
+            <select onChange={handleLevelChange} value={level} className='language-level-selector'>
+                    {levels.map(level => <option key={level} value={level}>{level}</option>)}
+            </select>
+        </div>
+    )
+}
+
+export default MyCVFormLanguageLevelSelector

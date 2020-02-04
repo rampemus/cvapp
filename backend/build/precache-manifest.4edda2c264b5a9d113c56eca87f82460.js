32f930f4c9d6ac8cf0f6334dec8fefb0282140b2
rampemus <pasi.toivanen@live.com>
2020-02-04 14:50:52 +0200

    Version 0.1 frontend build added

---
+++ backend/build/precache-manifest.4edda2c264b5a9d113c56eca87f82460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abce9776739118a5790695954c0d58f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d6f04350b17634a42f",
+    "url": "/static/css/main.65261b19.chunk.css"
+  },
+  {
+    "revision": "b3c2c3953d8122ca94b9",
+    "url": "/static/js/2.ef6ab380.chunk.js"
+  },
+  {
+    "revision": "62107ae5621e2b2270d15e42246c858a",
+    "url": "/static/js/2.ef6ab380.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5d6f04350b17634a42f",
+    "url": "/static/js/main.a7984a47.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);

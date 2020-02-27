ad56d829541e60a0ca590918bafc4476d82e3cf9
rampemus <pasi.toivanen@live.com>
2020-02-27 17:42:38 +0200

    Clean up ternary

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const PORT = process.env.PORT || 3004
-const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI
-    : ( process.env.NODE_ENV === 'development' ? process.env.MONGODB_URI_DEVELOPMENT : process.env.MONGODB_URI_TEST )
+const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI :
+    (process.env.NODE_ENV === 'development' ? process.env.MONGODB_URI_DEVELOPMENT :
+    process.env.MONGODB_URI_TEST )
 const JWT_SALT = process.env.JWT_SALT
 const ROOT_USERNAME = process.env.ROOT_USERNAME
 const ROOT_PASSWORD = process.env.ROOT_PASSWORD

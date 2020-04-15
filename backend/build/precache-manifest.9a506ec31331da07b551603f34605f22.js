10c6d4a0e5aa75c7bdaee7179b4db8db345c398c
rampemus <pasi.toivanen@live.com>
2020-04-16 00:05:56 +0300

    UI build

---
+++ backend/build/precache-manifest.9a506ec31331da07b551603f34605f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b850169e0319a06c20c4316324bb0f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac2323f6aca187553182",
+    "url": "/static/css/main.997c7f59.chunk.css"
+  },
+  {
+    "revision": "e93ee003b7dc0c5f4780",
+    "url": "/static/js/2.d943c920.chunk.js"
+  },
+  {
+    "revision": "fb6632bc327ba7d3bad296c087a91e17",
+    "url": "/static/js/2.d943c920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2323f6aca187553182",
+    "url": "/static/js/main.6698b872.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);

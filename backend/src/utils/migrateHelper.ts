d787e3d5e45b50b203c2eb95adc13833a7cfd8b4
rampemus <pasi.toivanen@live.com>
2020-07-29 19:32:28 +0300

    Iterate migration over all instances

--- backend/src/utils/migrateHelper.ts
+++ backend/src/utils/migrateHelper.ts
@@ -3,24 +3,29 @@ import Experience, { IExperience } from '../models/cv/experience'
 import Project, { IProject } from '../models/cv/project'
 import { MONGODB_URI, ROOT_USERNAME } from './config'
 
+// TODO: make these work with multiple instances in collections
 export const migrateExperienceDescriptionsToContent = async () => {
-  const experience: any = await Experience.findOne({ description: { $exists: true } })
-  const modifiedExperience = {
-    ...experience._doc,
-    content: [experience.description],
-    description: undefined,
-    owner: experience.owner,
+  const experiences: any = await Experience.find({ description: { $exists: true } })
+  for (const experience of experiences) {
+    const modifiedExperience = {
+      ...experience._doc,
+      content: [experience.description],
+      description: undefined,
+      owner: experience.owner,
+    }
+    await Experience.update({ _id: modifiedExperience._id }, modifiedExperience)
   }
-  await Experience.update({ _id: modifiedExperience._id }, modifiedExperience)
 }
 
 export const migrateProjectDescriptionsToContent = async () => {
-  const project: any = await Project.findOne({ description: { $exists: true } })
-  const modifiedProject = {
-    ...project._doc,
-    content: [project.description],
-    description: undefined,
-    owner: project.owner,
+  const projects: any = await Project.find({ description: { $exists: true } })
+  for (const project of projects) {
+    const modifiedProject = {
+      ...project._doc,
+      content: [project.description],
+      description: undefined,
+      owner: project.owner,
+    }
+    await Project.update({ _id: modifiedProject._id }, modifiedProject)
   }
-  await Project.update({ _id: modifiedProject._id }, modifiedProject)
 }
